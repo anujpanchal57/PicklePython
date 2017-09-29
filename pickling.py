6a8a0bb2de551a88a1960ce427060897c024256b
Anuj Panchal <anuj.panchal@somaiya.edu>
2017-09-29 14:22:50 +0530

    Update pickling.py

--- pickling.py
+++ pickling.py
@@ -1,33 +1,34 @@
+# Imported Pickle Module
 import pickle
 
-# imelda = ('More Mayhem', 'Imelda May', '2011',
-#           ((1, 'Pulling the rug'),
-#           (2, 'Psycho'),
-#           (3, 'Mayhem'),
-#           (4, 'Kentish House Waltz')))
-#
-# # This line writes the content into the 'imelda.pickle' file
-# with open("imelda.pickle", 'wb') as pickle_file:
-#     # This dumps the contents of imelda tuple in pickled file
-#     pickle.dump(imelda, pickle_file)
+imelda = ('More Mayhem', 'Imelda May', '2011',
+          ((1, 'Pulling the rug'),
+          (2, 'Psycho'),
+          (3, 'Mayhem'),
+          (4, 'Kentish House Waltz')))
 
-# # Reading the contents from IMELDA.PICKLE file
-# with open("imelda.pickle", 'rb') as imelda_pickled:
-#     # Loads the content in var IMELDA2
-#     imelda2 = pickle.load(imelda_pickled)
-#
-# print(imelda2)
-#
-# album, artist, year, track_list = imelda2
-#
-# print(album)
-# print(artist)
-# print(year)
-#
-# # Prints the track number and track titles in the output
-# for track in track_list:
-#     track_number, track_title = track
-#     print(track_number, track_title)
+# This line writes the content into the 'imelda.pickle' file
+with open("imelda.pickle", 'wb') as pickle_file:
+    # This dumps the contents of imelda tuple in pickled file
+    pickle.dump(imelda, pickle_file)
+
+# Reading the contents from IMELDA.PICKLE file
+with open("imelda.pickle", 'rb') as imelda_pickled:
+    # Loads the content in var IMELDA2
+    imelda2 = pickle.load(imelda_pickled)
+
+print(imelda2)
+
+album, artist, year, track_list = imelda2
+
+print(album)
+print(artist)
+print(year)
+
+# Prints the track number and track titles in the output
+for track in track_list:
+    track_number, track_title = track
+    print(track_number, track_title)
 
 imelda = ('More Mayhem', 'Imelda May', '2011',
           ((1, 'Pulling the rug'),
